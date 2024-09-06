80127f4573d42f5d7678b1808260963a38912dbc
Rohit Kumar Mahor <rk85783@gmail.com>
2024-09-06 11:55:38 +0530

    api authentication with laravel sanctum - done

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserAuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,9 @@ Route::get('/user', function (Request $request) {
 
 Route::post('login', [UserAuthController::class, 'login']);
 Route::post('signup', [UserAuthController::class, 'signup']);
+
+Route::group(['middleware' => "auth:sanctum"], function () {
+    Route::get('/profile', [UserController::class, 'getProfile']);
+});
+
+Route::get('login', [UserAuthController::class, 'login'])->name('login');
